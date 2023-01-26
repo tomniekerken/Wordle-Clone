bdd3dcea1366aeaa9ccee47b5f78cb030bc389fa
tomniekerken <104563013+tomniekerken@users.noreply.github.com>
2023-01-26 22:22:24 +0100

    Added some JS querySelectors

---
+++ script.js
@@ -0,0 +1,7 @@
+const keyArr = document.querySelectorAll('.key')
+
+const firstWordleInputArr = document.querySelectorAll('#first-wordle .wordle-input')
+const secondWordleInputArr = document.querySelectorAll('#second-wordle .wordle-input')
+const thirdWordleInputArr = document.querySelectorAll('#third-wordle .wordle-input')
+const fourthWordleInputArr = document.querySelectorAll('#fourth-wordle .wordle-input')
+const fifthWordleInputArr = document.querySelectorAll('#fifth-wordle .wordle-input')
