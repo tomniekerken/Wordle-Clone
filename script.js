67e9d8a670bb241bcb91f8438a4b3e5bdc7d0c38
tomniekerken <104563013+tomniekerken@users.noreply.github.com>
2023-01-29 19:09:26 +0100

    JS functionality for changing current chosen box for input

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ const thirdWordleInputArr = document.querySelectorAll('#third-wordle .wordle-inp
 const fourthWordleInputArr = document.querySelectorAll('#fourth-wordle .wordle-input')
 const fifthWordleInputArr = document.querySelectorAll('#fifth-wordle .wordle-input')
 
+const boxArr = document.querySelectorAll('.wordle-input')
+
+boxArr.forEach(box => {
+    box.addEventListener('click', () => {
+        for (let i = 0; i < boxArr.length; i++) {
+            const box = boxArr[i];
+            box.classList.remove('ready')
+        }
+        box.classList.add('ready')
+    })
+})
+
 keyArr.forEach(el => {
     el.addEventListener('click', () => {
         console.log(el.innerHTML)
