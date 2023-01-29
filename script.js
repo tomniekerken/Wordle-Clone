804bb3361a1bfdab27a58762c52c2115eed01668
tomniekerken <104563013+tomniekerken@users.noreply.github.com>
2023-01-29 20:27:03 +0100

    JS code improvements

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const keyArr = document.querySelectorAll('.key')
-
-const firstWordleInputArr = document.querySelectorAll('#first-wordle .wordle-input')
-const secondWordleInputArr = document.querySelectorAll('#second-wordle .wordle-input')
-const thirdWordleInputArr = document.querySelectorAll('#third-wordle .wordle-input')
-const fourthWordleInputArr = document.querySelectorAll('#fourth-wordle .wordle-input')
-const fifthWordleInputArr = document.querySelectorAll('#fifth-wordle .wordle-input')
-
 const boxArr = document.querySelectorAll('.wordle-input')
 
-boxArr.forEach(box => {
-    box.addEventListener('click', () => {
-        for (let i = 0; i < boxArr.length; i++) {
-            const box = boxArr[i];
-            box.classList.remove('ready')
-        }
-        box.classList.add('ready')
+const changeReadyWordle = (el) => {
+    for (let i = 0; i < boxArr.length; i++) {
+        const box = boxArr[i];
+        box.classList.remove('ready')
+    }
+    el.classList.add('ready')
+}
+
+const changeWordleInnerHTML = (el) => {
+    let ready = document.querySelector('.ready')
+    ready.innerHTML = el.innerHTML
+}
+
+boxArr.forEach(el => {
+    el.addEventListener('click', () => {
+        changeReadyWordle(el)
     })
 })
 
 keyArr.forEach(el => {
     el.addEventListener('click', () => {
-        let ready = document.querySelector('.ready')
-        ready.innerHTML = el.innerHTML
+        changeWordleInnerHTML(el)
     })
 });
