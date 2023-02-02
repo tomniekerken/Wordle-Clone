f37a11692c2294219755f8c5d161237b7b42a2cd
tomniekerken <104563013+tomniekerken@users.noreply.github.com>
2023-02-02 23:18:59 +0100

    Added functionality, cant change inactive wordle innerHTML

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ const keys = document.querySelectorAll('.key')
 
 const inputBoxes = document.querySelectorAll('.wordle-input')
 
-const changeReadyWordle = (el) => {
-    for (let i = 0; i < inputBoxes.length; i++) {
-        const box = inputBoxes[i];
-        box.classList.remove('ready')
-    }
+const changeReadyWordleOnClick = (el) => {
+    if (el.classList.contains('active')) {
+        for (let i = 0; i < inputBoxes.length; i++) {
+            const box = inputBoxes[i];
+            box.classList.remove('ready')
+        }
 
-    el.classList.add('ready')
+        el.classList.add('ready')
+    }
+}
+
+const changeReadyWordleOnKeyInput = () => {
+    let currentReady = document.querySelector('.ready')
+
+    // Array for each row
+    // check currentReady next sibling
+    // check if next sibling is inside row
+    // if not, stay on currentReady
+
 }
 
 const changeWordleInnerHTML = (el) => {
@@ -19,24 +31,26 @@ const changeWordleInnerHTML = (el) => {
     }
 
     if (el.innerHTML == 'Enter') {
-        // TODO check worlde world
+        // TODO check worlde word
         // TODO change active worlde row
     }
 
     if (el.innerHTML == 'Löschen') {
         ready.innerHTML = ''
+        // if ready.innerHTML was empty, move to sibling left of it if its inside the row
     }
 }
 
 keys.forEach(el => {
     el.addEventListener('click', () => {
         changeWordleInnerHTML(el)
+        changeReadyWordleOnKeyInput()
     })
 });
 
 inputBoxes.forEach(el => {
     el.addEventListener('click', () => {
-        changeReadyWordle(el)
+        changeReadyWordleOnClick(el)
     })
 })
 
