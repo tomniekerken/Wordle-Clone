1af157164aa41f5802c6b964891095369e160dad
tomniekerken <104563013+tomniekerken@users.noreply.github.com>
2023-02-03 20:09:27 +0100

    Added logic to change wordle on key input, but stay in same row

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const keys = document.querySelectorAll('.key')
 
 const inputBoxes = document.querySelectorAll('.wordle-input')
 
+const firstBoxes = document.querySelectorAll('#first-wordle .wordle-input')
+const secondBoxes = document.querySelectorAll('#second-wordle .wordle-input')
+const thirdBoxes = document.querySelectorAll('#third-wordle .wordle-input')
+const fourthBoxes = document.querySelectorAll('#fourth-wordle .wordle-input')
+const fifthBoxes = document.querySelectorAll('#fifth-wordle .wordle-input')
+
 const changeReadyWordleOnClick = (el) => {
     if (el.classList.contains('active')) {
         for (let i = 0; i < inputBoxes.length; i++) {
@@ -15,7 +21,14 @@ const changeReadyWordleOnClick = (el) => {
 
 const changeReadyWordleOnKeyInput = () => {
     let currentReady = document.querySelector('.ready')
+    let nextSibling = ''
 
+    for (let i = 0; i < firstBoxes.length; i++) {
+        if (currentReady == firstBoxes[i] && i + 1 <= firstBoxes.length) {
+            nextSibling = firstBoxes[i+1]
+            console.log(nextSibling)
+        }
+    }
     // Array for each row
     // check currentReady next sibling
     // check if next sibling is inside row
@@ -53,69 +66,3 @@ inputBoxes.forEach(el => {
         changeReadyWordleOnClick(el)
     })
 })
-
-
-
-/* 
-const keyArr = document.querySelectorAll('.key')
-const boxNodelist = document.querySelectorAll('.wordle-input')
-const boxArr = Array.from(boxNodelist)
-
-const changeReadyWordle = (el) => {
-    for (let i = 0; i < boxArr.length; i++) {
-        const box = boxArr[i];
-        box.classList.remove('ready')
-    }
-
-    // TODO: Check if Wordle .ready is inside current wrapper
-    // TODO: Remove possibility to add letters to different wordle wrapper
-
-    el.classList.add('ready')
-}
-
-const changeWordleInnerHTML = (el) => {
-    let ready = document.querySelector('.ready')
-
-    if (el.innerHTML != 'Enter' || el.innerHTML != 'Löschen') {
-        ready.innerHTML = el.innerHTML
-    }
-
-    if (el.innerHTML == 'Enter') {
-        // TODO change active worlde row
-    }
-
-    if (el.innerHTML == 'Löschen') {
-        ready.innerHTML = ''
-    }
-
-    nextReadyWorlde()
-}
-
-const nextReadyWorlde = () => {
-    let current = document.querySelector('.ready')
-    let next
-
-    for (let i = 0; i < boxArr.length; i++) {
-        if (current == boxArr[i]) {
-            next = boxArr[i+1]
-        }
-    }
-
-    changeReadyWordle(next)
-}
-
-const handleSubmit = () => {
-    // TODO: Proceed the complete user input for a wordle row
-}
-
-boxArr.forEach(el => {
-    el.addEventListener('click', () => {
-        changeReadyWordle(el)
-    })
-})
-
-keyArr.forEach(el => {
-    el.addEventListener('click', () => {
-        changeWordleInnerHTML(el)
-    })
-}); */
